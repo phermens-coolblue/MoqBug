d97687412db1183bafa1ae61682b179ca0b2de05
rikvandersanden <rik.van.der.sanden@gmail.com>
2017-01-04 15:37:58 +0100

    Change implementation to AmbientContext.DateTimeService

---
+++ MoqBug.Tests/Customizations/DateTimeFixture.cs
@@ -0,0 +1,24 @@
+﻿using AmbientContext.DateTimeService;
+
+using Moq;
+
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace MoqBug.Tests.Customizations
+{
+    public class DateTimeFixture
+    {
+        public readonly Mock<IDateTimeService> DateTimeMock = CreateDateTimeMock();
+
+        private static Mock<IDateTimeService> CreateDateTimeMock()
+        {
+            IFixture fixture = new Fixture().Customize(new AutoConfiguredMoqCustomization());
+
+            var dateTimeMock = fixture.Create<Mock<IDateTimeService>>();
+
+            AmbientDateTimeService.Create = () => new AmbientDateTimeService() { Instance = dateTimeMock.Object };
+            return dateTimeMock;
+        }
+    }
+}
